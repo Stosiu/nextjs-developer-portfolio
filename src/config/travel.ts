c9eb42c03362bdfa048abe2c08570d1c50d23872
Aleksander Stós <aleksander.stos@gmail.com>
2026-03-05 19:00:24 +0100

    feat: add 16 real travel photos, optimized to ~100KB each

--- src/config/travel.ts
+++ src/config/travel.ts
@@ -5,7 +5,20 @@ export type VisitedCountry = {
 };
 
 export const visitedCountries: VisitedCountry[] = [
-  {code: 'PL', year: 2000, image: '/images/travel/pl.jpg'},
-  {code: 'US', year: 2018, image: '/images/travel/us.jpg'},
-  {code: 'AU', year: 2023, image: '/images/travel/au.jpg'},
+  {code: 'AL', year: 2025, image: '/images/travel/al.jpg'},
+  {code: 'AU', year: 2019, image: '/images/travel/au.jpg'},
+  {code: 'BA', year: 2025, image: '/images/travel/ba.jpg'},
+  {code: 'CY', year: 2018, image: '/images/travel/cy.jpg'},
+  {code: 'GB', year: 2018, image: '/images/travel/gb.jpg'},
+  {code: 'GR', year: 2024, image: '/images/travel/gr.jpg'},
+  {code: 'HR', year: 2018, image: '/images/travel/hr.jpg'},
+  {code: 'IS', year: 2018, image: '/images/travel/is.jpg'},
+  {code: 'IT', year: 2025, image: '/images/travel/it.jpg'},
+  {code: 'MA', year: 2019, image: '/images/travel/ma.jpg'},
+  {code: 'ME', year: 2022, image: '/images/travel/me.jpg'},
+  {code: 'NO', year: 2023, image: '/images/travel/no.jpg'},
+  {code: 'RO', year: 2018, image: '/images/travel/ro.jpg'},
+  {code: 'SA', year: 2025, image: '/images/travel/sa.jpg'},
+  {code: 'AE', year: 2024, image: '/images/travel/ae.jpg'},
+  {code: 'US', year: 2019, image: '/images/travel/us.jpg'},
 ];
