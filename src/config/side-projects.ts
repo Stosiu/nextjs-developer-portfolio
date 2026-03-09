ef26d22314e4f4759a0c36693284ee0d9b7ce537
Aleksander Stós <aleksander.stos@gmail.com>
2026-03-09 22:07:06 +0100

    feat: add nickcheckr to footer side projects

--- src/config/side-projects.ts
+++ src/config/side-projects.ts
@@ -8,4 +8,8 @@ export const sideProjects: SideProject[] = [
     name: 'zdrofit.stosiu.dev',
     url: 'https://zdrofit.stosiu.dev',
   },
+  {
+    name: 'nickcheckr.stosiu.dev',
+    url: 'https://nickcheckr.stosiu.dev',
+  },
 ];
